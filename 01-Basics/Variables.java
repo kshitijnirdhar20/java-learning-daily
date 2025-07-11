f52cf5919dfe8437b886359d0a1bda5847aac0e9
KSHITIJ DINESH NIRDHAR <nirdharkshitij2006@gmail.com>
2025-07-11 22:42:04 +0530

    Add files via upload

---
+++ 01-Basics/Variables.java
@@ -0,0 +1,34 @@
+public class Variables {
+    public static void main(String[] args) {
+        int gta = 6;
+        String name = "KSHITIJ";
+        double pi = 3.14;
+        char gender = 'M';
+        boolean isAdmin = false;
+
+
+        System.out.println(gta);
+        System.out.println(name);
+        System.out.println(pi);
+        System.out.println(gender);
+        System.out.println(isAdmin);
+
+
+        System.out.println("The new game is going to launch is gta " + gta +" and the main the character's gender is "+ gender);
+
+
+        if(isAdmin) {
+            System.out.println("The pi value is: " + pi);
+        }
+        else {
+            System.out.println("The program not working");
+        }
+
+
+
+
+
+
+
+
+    }}
