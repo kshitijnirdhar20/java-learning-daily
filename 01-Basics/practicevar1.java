15b876b481dd977ac6b8072d1aec7b9bcce77a13
KSHITIJ DINESH NIRDHAR <nirdharkshitij2006@gmail.com>
2025-07-12 20:01:37 +0530

    Add files via upload

---
+++ 01-Basics/practicevar1.java
@@ -0,0 +1,16 @@
+public class practicevar1 {
+    public static void main(String[] args) {
+        int age = 20;
+        float height = 5.9f;
+        double weight = 65.5;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
+
